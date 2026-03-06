3841a8f3b64b61927d7695a841270c4f08453c62
eithen504 <eithen504@gmail.com>
2026-03-06 16:08:13 +0530

    using fetch request

--- src/api/session.ts
+++ src/api/session.ts
@@ -1,36 +1,68 @@
-import axiosInstance from "../lib/axios";
 import type { ProblemData } from "../types";
+
+const BASE_URL = "https://meet-mint-server.onrender.com/api";
 
 export const sessionApi = {
   createSession: async (data: ProblemData) => {
-    const response = await axiosInstance.post("/session", data);
-    return response.data;
+    const res = await fetch(`${BASE_URL}/session`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+      credentials: "include",
+    });
+
+    return res.json();
   },
 
   getActiveSessions: async () => {
-    const response = await axiosInstance.get("/session/active");
-    return response.data;
+    const res = await fetch(`${BASE_URL}/session/active`, {
+      credentials: "include",
+    });
+
+    return res.json();
   },
+
   getMyRecentSessions: async () => {
-    const response = await axiosInstance.get("/session/my-recent");
-    return response.data;
+    const res = await fetch(`${BASE_URL}/session/my-recent`, {
+      credentials: "include",
+    });
+
+    return res.json();
   },
 
   getSessionById: async (id: string) => {
-    const response = await axiosInstance.get(`/session/${id}`);
-    return response.data;
+    const res = await fetch(`${BASE_URL}/session/${id}`, {
+      credentials: "include",
+    });
+
+    return res.json();
   },
 
   joinSession: async (id: string) => {
-    const response = await axiosInstance.post(`/session/${id}/join`);
-    return response.data;
+    const res = await fetch(`${BASE_URL}/session/${id}/join`, {
+      method: "POST",
+      credentials: "include",
+    });
+
+    return res.json();
   },
+
   endSession: async (id: string) => {
-    const response = await axiosInstance.post(`/session/${id}/end`);
-    return response.data;
+    const res = await fetch(`${BASE_URL}/session/${id}/end`, {
+      method: "POST",
+      credentials: "include",
+    });
+
+    return res.json();
   },
+
   getStreamToken: async () => {
-    const response = await axiosInstance.get(`/chat/token`);
-    return response.data;
+    const res = await fetch(`${BASE_URL}/chat/token`, {
+      credentials: "include",
+    });
+
+    return res.json();
   },
 };
